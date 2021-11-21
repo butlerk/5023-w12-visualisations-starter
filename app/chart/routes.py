43e81d3f7ba29aa2aa573ab19afc8ded71ab0023
Kelly Butler <butlerk@bhs.vic.edu.au>
2021-11-21 20:14:30 +1100

    Added two charts
    Books read by user & books by genre

--- app/chart/routes.py
+++ app/chart/routes.py
@@ -8,7 +8,7 @@ import plotly
 
 from app import db
 from app.chart import bp
-from app.models import Book
+from app.models import Book, User
 
 @bp.route('/')
 @login_required
@@ -39,6 +39,7 @@ def user_books_chart():
         "JOIN user u on ub.user_id = u.id "
         "GROUP BY username"
     )
+
     df = pd.read_sql(query, db.session.bind)
 
     # Draw the chart and dump it into JSON format
@@ -50,4 +51,36 @@ def user_books_chart():
 
 # TODO: Add a route for a bar chart that compares books read per year by user
 
-# TODO: Add a route for a bar chart that compares number of books per genre
+@bp.route('/books_read_by_user')
+@login_required
+def books_read_by_user_chart():
+    user_query = User.query
+    df = pd.read_sql(user_query.statement, user_query.session.bind)
+
+    # Draw the chart and dump it into JSON format
+    chart = px.bar(df, x='username', y='books_read_per_year')
+    chart_JSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder, indent=4)
+
+    # Returns the template, including the JSON data for the chart
+    return render_template('chart_page.html', title = 'Books Read by User by Year', chart_JSON = chart_JSON)
+
+
+@bp.route('/books_by_genre')
+@login_required
+def books_by_genre_chart():
+    # Run query to get count of books owned per user and load into DataFrame
+    query = (
+        "SELECT name, count(*) as genre_count "
+        "FROM book b "
+        "JOIN genre g ON b.genre_id = g.id "
+        "GROUP BY name "
+    )
+    df = pd.read_sql(query, db.session.bind)
+    print(df)
+    # Draw the chart and dump it into JSON format
+    chart = px.bar(df, x ='name', y='genre_count', labels=
+        {"name": "Genre","genre_count":"Number in each Genre"})
+    chart_JSON = json.dumps(chart, cls=plotly.utils.PlotlyJSONEncoder, indent=4)
+
+     # Returns the template, including the JSON data for the chart
+    return render_template('chart_page.html', title = 'Books by Genre', chart_JSON = chart_JSON)
